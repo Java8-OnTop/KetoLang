ba77075bb529782a403a5927b3543fa903f55057
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-09-21 19:55:58 -0500

    bbnb

--- samples/cli/src/main/kotlin/com/pushtorefresh/ketolang/sample/A.kt
+++ samples/cli/src/main/kotlin/com/pushtorefresh/ketolang/sample/A.kt
@@ -1,6 +1,6 @@
 package com.pushtorefresh.ketolang.sample
 
-// fib(n) = fib(n - 1) + fib(n - 2)
+//h fib(n) = fib(n - 1) + fib(n - 2)
 fun naiveFibonacci(n: Int): Int {
     if (n == 0 || n == 1) {
         return n
